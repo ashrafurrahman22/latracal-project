ab65b823e64113936ccdaceaeeda3f5b70300800
ashrafur22 <ashrafurrahman22@gmail.com>
2022-08-28 22:47:01 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import RequireAuth from "./Pages/Login/RequireAuth";
-import Login from "./Pages/Login/Login";
-import SignUpMain from "./Pages/Login/SignUpMain";
 import Navbar from "./Pages/Home/Navbar";
 import './App.css';
 
@@ -15,15 +12,11 @@ function App() {
       <div className='flex justify-center items-center w-full bg-slate-700'>
       <Routes>
           <Route path="/" element={
-            <RequireAuth>
             <Home></Home>
-            </RequireAuth>
             }></Route>
           <Route path="/note/:notesId" element={
           <Home></Home>
           }></Route>
-          <Route path="/login" element={<Login></Login>}></Route>
-          <Route path="/signup" element={<SignUpMain></SignUpMain>}></Route>
       </Routes>
      
 
