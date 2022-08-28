4d223a6ef9f320a9c6b3a550c5243d6ef5e57654
ashrafur22 <ashrafurrahman22@gmail.com>
2022-08-28 22:50:32 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Navbar from "./Pages/Home/Navbar";
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
       <div className='flex justify-center items-center w-full bg-slate-700'>
       <Routes>
           <Route path="/" element={
